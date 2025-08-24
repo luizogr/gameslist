c49f48212f74121c14550f76ba9ca0dc40d18af8
luizogr <luizpkk@yahoo.com.br>
2025-08-24 05:35:51 -0300

    Get games in list, search SQL and projection

--- src/main/java/com/devsuperior/gameslist/services/GameService.java
+++ src/main/java/com/devsuperior/gameslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.gameslist.services;
 import com.devsuperior.gameslist.dto.GameDTO;
 import com.devsuperior.gameslist.dto.GameMinDTO;
 import com.devsuperior.gameslist.entities.Game;
+import com.devsuperior.gameslist.projections.GameMinProjection;
 import com.devsuperior.gameslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
